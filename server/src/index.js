15952995e2c814c2ffc8e5e595ad5c7720797767
Stefan Huckschlag <stefan.huckschlag@web.de>
2020-11-05 14:09:32 +0100

    chore(server): integrate cors middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./routes/router');
 const mongoose = require('./models/index');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json()); // parse incoming Data
 app.use(router); // connect router
 
